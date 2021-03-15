f3c2679068fda7239673ffcf2be92146f44e4eb8
Ankit mehta <ankit.mehta@bakbuck.com>
2021-03-15 19:36:47 +0530

    notification

---
+++ android/app/src/main/kotlin/com/ujjwal/app/Application.java
@@ -0,0 +1,33 @@
+package com.ujjwal.app;
+
+
+import com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+import io.flutter.plugins.pathprovider.PathProviderPlugin;
+import io.flutter.view.FlutterMain;
+
+
+public class Application extends FlutterApplication implements PluginRegistry.PluginRegistrantCallback {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FlutterFirebaseMessagingService.setPluginRegistrant(this);
+        FlutterMain.startInitialization(this);
+
+    }
+
+
+
+    @Override
+    public void registerWith(PluginRegistry registry) {
+        FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+        FlutterLocalNotificationsPlugin.registerWith(registry.registrarFor("com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin"));
+        PathProviderPlugin.registerWith(registry.registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
+
+    }
+}
